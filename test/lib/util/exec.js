7acd76042b21e30adb63ffb4ad2e0e0e4019f904
Michael "Z" Goddard <mzgoddard@gmail.com>
2015-01-07 12:31:15 -0500

    Output exec errors on log if given as option.
    - Add exec.throwIfAnyExitedNonZero helper.

--- test/lib/util/exec.js
+++ test/lib/util/exec.js
@@ -2,16 +2,33 @@ const expect = require('chai').expect;
 const exec = require('../../../lib/util/exec');
 const fs = require('fs');
 
-describe.skip('exec', function() {
+describe('exec', function() {
 
   it('should resolve to the stdout/code of a command', function() {
     return exec('cat README.md').then(function(result) {
-      expect(result).to.deep.equal({
-        code: 0,
-        err: null,
-        output: fs.readFileSync('./README.md', 'utf8')
-      });
+      expect(result.code).to.equal(0);
+      expect(result.output).to.equal(fs.readFileSync('./README.md', 'utf8'));
     });
+  });
+
+  describe('#throwIfAnyExitedNonZero', function() {
+
+    it('should return results if all exited with code 0', function() {
+      var results = [{ code: 0 }];
+      expect(exec.throwIfAnyExitedNonZero(results)).to.equal(results);
+    });
+
+    it('should throw if any exited non zero', function() {
+      var results = [{
+        code: 1,
+        command: 'FAKE',
+        opts: {},
+      }];
+      var log = function() {};
+      expect(exec.throwIfAnyExitedNonZero.bind(null, results, log))
+        .to.throw(Error);
+    });
+
   });
 
   // more tests needed
